f0f3245421cd61468264e52cdc3462be72838132
TheDerpGamer <garret2kool@gmail.com>
2020-01-21 10:38:58 -0700

    Update Triton.java

--- src/main/java/thederpgamer/astralspirits/entity/spirit/Triton.java
+++ src/main/java/thederpgamer/astralspirits/entity/spirit/Triton.java
@@ -6,8 +6,10 @@ public class Triton extends AstralSpirit {
 
     public Triton(World worldIn) {
         super(worldIn);
+        setHeight(2.0F);
+        setWidth(0.75F);
         setMaxHealth(25.0D);
-        
+        setArmor(5.0D);
         applyEntityAttributes();
     }
 }
