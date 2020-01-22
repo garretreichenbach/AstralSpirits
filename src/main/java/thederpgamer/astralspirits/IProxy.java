cf67e92effc23e27f4984919e1af30e380fa2205
TheDerpGamer <garret2kool@gmail.com>
2020-01-22 12:42:01 -0700

    Create IProxy.java

---
+++ src/main/java/thederpgamer/astralspirits/IProxy.java
@@ -0,0 +1,16 @@
+package thederpgamer.astralspirits;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+public interface IProxy {
+	void preInit(FMLPreInitializationEvent event);
+
+	void init(FMLInitializationEvent event);
+
+	void postInit(FMLPostInitializationEvent event);
+
+	void serverLoad(FMLServerStartingEvent event);
+}
