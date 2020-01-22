7f48ae7e6ea65773b410c5d070d7e41541b60afd
TheDerpGamer <garret2kool@gmail.com>
2020-01-22 12:41:52 -0700

    Create ElementType.java

---
+++ src/main/java/thederpgamer/astralspirits/entity/spirit/ElementType.java
@@ -0,0 +1,12 @@
+package thederpgamer.astralspirits.entity.spirit;
+
+import net.minecraft.world.BossInfo;
+
+public enum ElementType {
+    WATER,
+    EARTH,
+    ELECTRIC,
+    FIRE,
+    MYSTIC,
+    AIR
+}
