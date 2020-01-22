39c4c65cc96aa1b1d5b9c72b4bd9941394561b9b
TheDerpGamer <garret2kool@gmail.com>
2020-01-22 12:42:04 -0700

    Update Main.java

--- src/main/java/thederpgamer/astralspirits/Main.java
+++ src/main/java/thederpgamer/astralspirits/Main.java
@@ -1,18 +1,22 @@
 package thederpgamer.astralspirits;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
-public class Main
-{
+public class Main {
     public static final String MODID = "astralspirits";
     public static final String NAME = "Astral Spirits";
     public static final String VERSION = "0.1.0";
+    public static final SimpleNetworkWrapper PACKET_HANDLER = NetworkRegistry.INSTANCE.newSimpleChannel("astralspirits");
+    @SidedProxy(clientSide = "thederpgamer.astralspirits.ClientProxy", serverSide = "thederpgamer.astralspirits.ServerProxy")
+    public static IProxy proxy;
 
     private static Logger logger;
 
