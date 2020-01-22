fc5244ef1eefc43cd8812be2d787f3d2abcc9fa8
TheDerpGamer <garret2kool@gmail.com>
2020-01-22 13:15:56 -0700

    Create Global.java

---
+++ src/main/java/thederpgamer/astralspirits/Global.java
@@ -0,0 +1,11 @@
+package thederpgamer.astralspirits;
+
+import thederpgamer.astralspirits.entity.spirit.AstralSpirit;
+import thederpgamer.astralspirits.entity.spirit.ability.SpiritAbility;
+import java.util.List;
+
+public class Global {
+
+    public static List<AstralSpirit> spirits;
+    public static List<SpiritAbility> abilities;
+}
