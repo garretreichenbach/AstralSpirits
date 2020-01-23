5d3cd58bbf901716725798eae3d737b3394612cb
TheDerpGamer <garret2kool@gmail.com>
2020-01-23 10:44:55 -0700

    Update BlowDown.java

--- src/main/java/thederpgamer/astralspirits/entity/spirit/ability/BlowDown.java
+++ src/main/java/thederpgamer/astralspirits/entity/spirit/ability/BlowDown.java
@@ -1,14 +1,16 @@
 package thederpgamer.astralspirits.entity.spirit.ability;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
 import thederpgamer.astralspirits.entity.spirit.AstralSpirit;
 
 public class BlowDown extends SpiritAbility {
 
     @Override
-    public void activateAbility(AstralSpirit spirit, Entity[] target) {
+    public void activateAbility(AstralSpirit spirit, Entity target) {
+        World world = spirit.getEntityWorld();
         spirit.setImmuneToExplosions(true);
-        //TODO:Create explosion at spirit position
+        world.createExplosion(target, spirit.posX, spirit.posY, spirit.posZ, 3.0F, false);
         spirit.setImmuneToExplosions(false);
     }
 }
